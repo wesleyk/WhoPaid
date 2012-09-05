1064491d5a9a93227223531dca3735e3cf1b4862
Wesley Kim <wesleyjkim@gmail.com>
2012-09-05 15:45:29 -0400

    testing mongodb

--- WhoPaid.py
+++ WhoPaid.py
@@ -1,11 +1,37 @@
+import os
+from flask import Flask
 from twilio.rest import TwilioRestClient
+from pymongo import Connection
 
+app = Flask(__name__)
+
+# Twilio details
 account = "AC74068c46306d722c23fc68291b67071a"
 token = "da09cf1ce50760e7ef4405d9c8334239"
 client = TwilioRestClient(account, token)
 
-for message in client.sms.messages.list():
-    print message.body
+# Connect to MongoDB, and retrieve collections
+connection = Connection()
+db = connection.app7324197
+users = db.users
+payments = db.payments
 
-message = client.sms.messages.create(to="+14254436511", from_="+14259678372",
-                                     body="Hello there!")
+
+@app.route('/')
+def hello():
+	output = ""
+	for user in users.find():
+		output += user + " "
+
+	#for message in client.sms.messages.list():
+	#    print message.body
+
+	#message = client.sms.messages.create(to="+14254436511", from_="+14259678372",
+	#                                     body="Hello there!")
+	
+    return output
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
