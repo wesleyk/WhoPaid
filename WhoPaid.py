01104be1bada33d68bf34732e4965c170ab45070
Wesley Kim <wesleyjkim@gmail.com>
2012-09-05 23:15:21 -0400

    test sms sending

--- WhoPaid.py
+++ WhoPaid.py
@@ -25,6 +25,7 @@ def parseSMS():
 	# first message is the most recent
 	for message in client.sms.messages.list():
 	    body = message.body
+		break
 	
 	message = client.sms.messages.create(to="+14254436511", from_="+14259678372", body=body)
 
