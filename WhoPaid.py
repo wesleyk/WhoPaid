b15c2394e6d142ec45e9e3e2464503253addd64d
Wesley Kim <wesleyjkim@gmail.com>
2012-09-05 20:51:07 -0400

    twilio changes

--- WhoPaid.py
+++ WhoPaid.py
@@ -5,17 +5,16 @@ from pymongo import Connection
 
 app = Flask(__name__)
 
-# Twilio details
-account = "AC74068c46306d722c23fc68291b67071a"
-token = "da09cf1ce50760e7ef4405d9c8334239"
-client = TwilioRestClient(account, token)
-
 # Connect to MongoDB, and retrieve collections
 MONGO_URL = os.environ.get('MONGOHQ_URL')
 
-
 @app.route('/')
 def parseSMS():
+	# Twilio details
+	account = "AC74068c46306d722c23fc68291b67071a"
+	token = "da09cf1ce50760e7ef4405d9c8334239"
+	client = TwilioRestClient(account, token)
+	
 	connection = Connection(MONGO_URL)
 	db = connection.app7324197
 	
@@ -23,12 +22,12 @@ def parseSMS():
 	payments = db.payments
 	
 	for message in client.sms.messages.list():
-	    return message.body
+	    body = message.body
 	
 	message = client.sms.messages.create(to="+14254436511", from_="+14259678372",
-		                                     body="Hello there!")
+		                                     body=body)
 		
-	return "Hello, world!"
+	return
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
